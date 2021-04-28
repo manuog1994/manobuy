203f031d24d34be497ff427294d9ade7160a69ec
Manuel Ortega Galiano <manuelortegagaliano@MacBook-Pro-de-Manuel.local>
2021-04-29 00:23:05 +0200

    refix slider

--- resources/views/announcements/_slider.blade.php
+++ resources/views/announcements/_slider.blade.php
@@ -1,22 +1,16 @@
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
-
-@foreach($announcement->images as $image)
-
-        @if($announcement->images->count() == 0)
-        <div class="carousel-item active">
-            <img src="{{$announcement->img}}500" class="d-block w-50 m-auto" alt="{{$announcement->name}}">
-        </div>
-
-@endforeach
-
-        @else
         @foreach($announcement->images as $image)
-        <div class="carousel-item @if($loop->first) active @endif">
-            <img src="{{$image->getUrl(800,500)}}" class="d-block w-50 m-auto" alt="{{$announcement->name}}">
-        </div>
+            @if($announcement->images->count() == 0)
+                <div class="carousel-item active">
+                    <img src="{{$announcement->img}}500" class="d-block w-50 m-auto" alt="{{$announcement->name}}">
+                </div>
+            @else
+                <div class="carousel-item @if($loop->first) active @endif">
+                    <img src="{{$image->getUrl(800,500)}}" class="d-block w-50 m-auto" alt="{{$announcement->name}}">
+                </div>
+            @endif
         @endforeach
-        @endif
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
